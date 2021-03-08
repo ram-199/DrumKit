2e2e73737aed2bd0eb499ff915d5bfbfc2761fa9
ram-199 <ramu36985@gmail.com>
2021-03-08 09:55:01 +0530

    few changes

--- src/js/index.js
+++ src/js/index.js
@@ -27,32 +27,32 @@ function buttonAnimation(key){
 
 function makeSound(key){
     switch (key) {
-        case "w":
-            var w = new Audio ("sounds/tom-1.mp3");
+        case "a":
+            var w = new Audio ("src/sounds/tom-1.mp3");
             w.play();
             break;
-        case "a":
-            var a = new Audio ("sounds/tom-2.mp3");
+        case "s":
+            var a = new Audio ("src/sounds/tom-2.mp3");
             a.play();
             break;
-        case "s":
-            var s = new Audio ("sounds/tom-3.mp3");
+        case "d":
+            var s = new Audio ("src/sounds/tom-3.mp3");
             s.play();
             break;
-        case "d":
-            var d = new Audio ("sounds/tom-4.mp3");
+        case "f":
+            var d = new Audio ("src/sounds/tom-4.mp3");
             d.play();
             break;
         case "j" :
-            var j = new Audio ("sounds/snare.mp3");
+            var j = new Audio ("src/sounds/snare.mp3");
             j.play();
             break;
         case "k":
-            var k = new Audio ("sounds/crash.mp3");
+            var k = new Audio ("src/sounds/crash.mp3");
             k.play();
             break;
         case "l":
-            var l = new Audio ("sounds/kick-bass.mp3");
+            var l = new Audio ("src/sounds/kick-bass.mp3");
             l.play();
             break;
         default:
